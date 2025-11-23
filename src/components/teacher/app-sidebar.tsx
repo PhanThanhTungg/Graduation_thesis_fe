ba48511fe3bfab15a2bdeae1d562afba62e161b0
SA - Tùng PT1 <tungpt1@bsscommerce.com>
2025-11-23 10:40:28 +0700

    feat(finance): implement Finance page for teachers with wallet and transaction management

--- src/components/teacher/app-sidebar.tsx
+++ src/components/teacher/app-sidebar.tsx
@@ -1,11 +1,30 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { IconChartBar, IconDashboard, IconFolder, IconHelp, IconListDetails, IconSearch, IconSettings, IconUser, IconUsers, IconWallet } from "@tabler/icons-react"
-import { NavMain } from "@/components/teacher/nav-main"
-import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar"
-import Link from "next/link"
-import { ArrowLeftToLine } from "lucide-react"
+import * as React from "react";
+import {
+  IconChartBar,
+  IconDashboard,
+  IconFolder,
+  IconHelp,
+  IconListDetails,
+  IconSearch,
+  IconSettings,
+  IconUser,
+  IconUsers,
+  IconWallet,
+} from "@tabler/icons-react";
+import { NavMain } from "@/components/teacher/nav-main";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import Link from "next/link";
+import { ArrowLeftToLine } from "lucide-react";
 
 const data = {
   user: {
@@ -46,9 +65,9 @@ const data = {
     },
     {
       title: "Finance",
-      url: "#",
-      icon: IconWallet
-    }
+      url: "/teacher/Finance",
+      icon: IconWallet,
+    },
   ],
   navSecondary: [
     {
@@ -67,7 +86,7 @@ const data = {
       icon: IconSearch,
     },
   ],
-}
+};
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
@@ -103,5 +122,5 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         </SidebarMenu>
       </SidebarFooter>
     </Sidebar>
-  )
+  );
 }
